26df5ab368f8164efc215f5f6a6a59011c837d34
denis6898 <153389136+denis6898@users.noreply.github.com>
2023-12-11 19:33:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-function checkTheme() {
-    const savedTheme = localStorage.getItem('siteTheme');
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-theme');
-    } else {
-        document.body.classList.remove('dark-theme');
-    }
 
-    const servicesSection = document.querySelector('.services-section');
-    if (document.body.classList.contains('dark-theme') && servicesSection) {
-        servicesSection.classList.add('dark-theme');
-    } else if (servicesSection) {
-        servicesSection.classList.remove('dark-theme');
-    }
-}
-
-function activateDarkMode() {
-    document.body.classList.add('dark-theme');
-    localStorage.setItem('siteTheme', 'dark');
-    checkTheme();
-}
-
-function deactivateDarkMode() {
-    document.body.classList.remove('dark-theme');
-    localStorage.setItem('siteTheme', 'light');
-    checkTheme();
-}
 
 function showServiceInfo(infoId) {
     const allInfos = document.querySelectorAll('.service-info');
@@ -40,8 +14,3 @@ function redirectToSite(url) {
 
 document.addEventListener('DOMContentLoaded', checkTheme);
 
-// Eliminarea butoanelor de schimbare temă
-const themeButtons = document.querySelector('.theme-buttons');
-if (themeButtons) {
-    themeButtons.innerHTML = '';
-}
