34cc8250192db9ef054a74373681b503c3f62398
denis6898 <153389136+denis6898@users.noreply.github.com>
2023-12-11 18:39:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ function checkTheme() {
     } else {
         document.body.classList.remove('dark-theme');
     }
+
+    const servicesSection = document.querySelector('.services-section');
+    if (document.body.classList.contains('dark-theme')) {
+        servicesSection.classList.add('dark-theme');
+    } else {
+        servicesSection.classList.remove('dark-theme');
+    }
 }
 
 function activateDarkMode() {
     document.body.classList.add('dark-theme');
     localStorage.setItem('siteTheme', 'dark');
     checkTheme();
+}
+
+function deactivateDarkMode() {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('siteTheme', 'light');
+    checkTheme();
+}
+
+function toggleTheme() {
+    if (document.body.classList.contains('dark-theme')) {
+        deactivateDarkMode();
+    } else {
+        activateDarkMode();
+    }
 }
 
 function showServiceInfo(infoId) {
@@ -27,4 +48,11 @@ function redirectToSite(url) {
 
 document.addEventListener('DOMContentLoaded', checkTheme);
 
+const themeButtons = document.createElement('div');
+themeButtons.classList.add('theme-buttons');
+themeButtons.innerHTML = `
+    <button class="dark-mode" onclick="toggleTheme()">Dark Mode</button>
+    <button class="light-mode" onclick="toggleTheme()">Light Mode</button>
+`;
 
+document.body.appendChild(themeButtons);
