0b53e7ea6fa489c85d65cd0da22bbadc75dacbf7
denis6898 <153389136+denis6898@users.noreply.github.com>
2023-12-12 01:00:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function checkTheme() {
+    const savedTheme = localStorage.getItem('siteTheme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+
+    const servicesSection = document.querySelector('.services-section');
+    if (document.body.classList.contains('dark-theme') && servicesSection) {
+        servicesSection.classList.add('dark-theme');
+    } else if (servicesSection) {
+        servicesSection.classList.remove('dark-theme');
+    }
+}
 
 
 function showServiceInfo(infoId) {
